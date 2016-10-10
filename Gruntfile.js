5445df4b1705231f63ab539e588980be514d8f32
NickKo <nkorostelev@gmail.com>
2016-10-10 23:52:43 +0300

    some refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
-module.exports = function(grunt){
-    grunt.initConfig({
-        pkg:grunt.file.readJSON('package.json'),
+module.exports = function(grunt) {
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         watch:{
             options:{livereload:true},
             files:['public/**','server/**. *.*'],
             tasks:[]
         },
-          express:{
-              all:{
-                  options:{
-                      port:3000,
-                      hostname:'localhost',
-                      bases:['./'],
-                      livereload:true
-                  }
-              }
-          }
+        express: {
+            all: {
+                options: {
+                  port:3000,
+                  hostname:'localhost',
+                  bases:['./'],
+                  livereload:true
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-express');
-    grunt.registerTask('server',['express','watch']);
+    grunt.registerTask('server', ['express', 'watch']);
 
-    };
+};
