e68c0fb2d0ec639fe088afa9f92016663585ba3b
Nikita Korostelev <nkorostelev@gmail.com>
2024-11-08 11:02:03 +0000

    (core) bump npm packages

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import Layer from './layer';
 import Snake from './snake';
-import ml5 from 'ml5';
 import '../scss/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -21,32 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
         layer.init();
         snake.init();
-
-        // sound controller
-        const classifierOptions = {
-            probabilityThreshold: 0.9,
-        };
-
-        const classifier = ml5.soundClassifier(
-            'SpeechCommands18w',
-            classifierOptions,
-            modelReady
-        );
-
-        function modelReady() {
-            classifier.classify((error, result) => {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-
-                const commands = ['left', 'down', 'up', 'right'];
-                const command = result[0].label;
-                if (commands.includes(command)) {
-                    snake.move(command);
-                }
-            });
-        }
     } catch (e) {
         console.error(e);
         alert(e.message);
